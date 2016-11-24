f310b4973b3d72113b53f3a2f0574ae0138b6dee
gerrity95 <gerrity95@gmail.com>
2016-11-24 00:52:57 +0000

    Adding the queries page - a page to hold all our SQL queries

---
+++ backend/queries.php
@@ -0,0 +1,12 @@
+<?php
+
+/* Login Related Queries - queries used in loginserv.php */
+
+/* Register related queries - queries used in registerserv.php */
+
+/* queries used in check.php - to see if the user is already logged in */
+
+/* */
+
+
+?>
