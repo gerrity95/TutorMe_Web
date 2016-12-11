3baf18ae1f0df72d1be635b0e64364ae965ecb32
gerrity95 <gerrity95@gmail.com>
2016-12-11 18:27:00 +0000

    Began Secondary registration form Seperated for both students and tutors

---
+++ js/general.js
@@ -0,0 +1,3 @@
+$(document).on("click","#register_link",function(){
+ alert("Not so fast. You must fill out the form first.");
+});
--- register_full.php
+++ register_full.php
@@ -1,11 +1,104 @@
 <!DOCTYPE html>
 <html>
 <head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- Bootstrap stylesheets -->
+  <title>TutorMe Registration Finish</title>
+<!-- To incorporate Lato - a Google font -->
+ <link href='http://fonts.googleapis.com/css?family=Lato:400,700' rel='stylesheet'  type='text/css'>
+
+<!-- For social media buttons -->
+<!-- The stylesheet where we add our own stylings -->
+<link rel='stylesheet prefetch' href='http:////netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css'>
+
+
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+<link rel="stylesheet" href="css/my_style.css">
+<link rel="stylesheet" href="social_media/css/button_style.css">
+<link rel="stylesheet" href="css/animate.css">
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<script type="text/javascript" src="js/html_reference.js"></script>
+<script type="text/javascript" src="js/general.js"></script>
 
 
 </head>
 
-<body>
+<body style="background:#ffffff">
+  <nav id="myNavbar" class="navbar navbar-default navbar-inverse navbar-fixed-top" role="navigation">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbarCollapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+
+      <a href="index.html"><img src="images/smallLogoWhite.png" style="padding-right:50px"></a>
+        </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="nav navbar-nav">
+                <li class=""><a id="register_link" href="#"><span style="padding-right:10px" class="glyphicon glyphicon-home"></span>Home</a></li>
+                <li><a id="register_link" href="#"><span style="padding-right:10px" class="glyphicon glyphicon-envelope"></span>Messages</a></li>
+                <li><a id="register_link" href="#"><span style="padding-right:10px" class="glyphicon glyphicon-calendar"></span>Schedule</a></li>
+            </ul>
+      <ul class="nav navbar-nav navbar-right">
+
+        <li class="dropdown">
+          <a href="#" style="padding-right:20px" class="dropdown-toggle" data-toggle="dropdown"><span style="padding-right:10px" class="glyphicon glyphicon-user"></span>Joe Bloggs</a>
+          <ul style="" class="dropdown-menu">
+            <li><a href="student_how.html">Settings</a></li>
+            <li><a href="tutor_how.html">Logout</a></li>
+          </ul>
+        </li>
+
+        </div>
+      </ul>
+    </div>
+  </div>
+  </nav>
+
+  <div class="jumbotron ">
+      <div class="container-fluid">
+  	<div class="page-header">
+  		<h1>TutorMe <small style="font-family: 'Source Sans Pro', sans-serif;">Find the perfect tutor.</small></h1>
+  	</div>
+          <p style="" class="lead">Welcome to TutorMe. Before we continue we must get a few more details from you.</p>
+
+      </div>
+  </div>
+
+  <div class="header-2">
+  	<div class="page-header header-filter" style="background-color:#ffffff;background-size:cover;min-height:750px">
+  	<div class="container-fluid">
+  		<div class="row">
+  		<div style="margin-top:60px" class="col-sm-6 col-md-4 col-lg-4 col-xs-offset-1 ">
+  			<center><img class="feature_images" src="images/LogoLarge.png" /></center>
+  			<h2 class="general_text">Finding you the perfect Tutor </h2>
+  			<h2><small style="color:black;font-family: 'Lato', sans-serif;">Fill out this short form and you'll be ready to go.</small> </h2>
+  		</div>
+
+  		<div style="margin-top:50px;" class="col-sm-6 col-md-4 col-lg-4 col-xs-offset-1">
+  			<div refer-HTML="tutor_register.html">
+
+        </div>
+        <script>
+        referHTML()
+        </script>
+  		</div>
+
+
+  	 </div>
+  	</div>
+    </div>
+  </div>
 
 
 </body>
