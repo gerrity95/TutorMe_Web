ce72ed588719e6fa65b0f2eba274596c26291a5b
gerrity95 <gerrity95@gmail.com>
2017-01-30 12:26:14 +0000

    Checkbox working for JC, new table created in DB

--- checkbox_test.php
+++ checkbox_test.php
@@ -1,11 +1,13 @@
 <?php
-include("backend/connection.php");
+//include("backend/connection.php");
+include("checkbox_testForm.php");
 
 $sql = $conn->query("SELECT subject_id, subject_name from Subjects WHERE cycle_id = '1' ORDER BY  `Subjects`.`subject_name` ASC ");
 
 echo "<html>";
     echo "<body>";
     echo "<center><h4>From DB</h4></center>";
+    echo "<form action='' method='POST' style=''>";
     echo "<div class='checkbox checkbox-info checkbox_scroller' style='height:150px;width:350px;'>";
     //echo "<select style='' name='location' class='selectpicker show-tick' data-live-search='true'>";
 
@@ -16,47 +18,38 @@ echo "<html>";
                       $name = $row['subject_name'];
                       echo '<input id="'.$name.'" name="checkso[]" type="checkbox" value="'.$id.'"><label for="'.$name.'">'.$name.'</label><br>';
     }
-
         echo "</div>";
+        echo "<center><input onClick='' type='submit' value='Submit' name='submit'></center>";
+        echo "</form>";
         echo "</body>";
         echo "</html>";
 
-$test = "hello";
-
 ?>
 
-<body style="background:#dbdfe5">
-  <!--
-<center><h4>Manual Input</h4></center>
-<form action="" method="POST">
-                <div class="checkbox checkbox-info checkbox_scroller" style="height: 150px;width:350px;">
-    <input id="jc1" name="" type="checkbox"><label for="jc1">Art </label><br>
-    <input id="jc2" name="" type="checkbox"><label for="jc2">Business Studies </label><br>
-    <input id="jc3" name="" type="checkbox"><label for="jc3">CSPE </label><br>
-    <input id="jc4" name="" type="checkbox"><label for="jc4">English </label><br>
-    <input id="jc5" name="" type="checkbox"><label for="jc5">French </label><br>
-    <input id="jc6" name="" type="checkbox"><label for="jc6">Geography </label><br>
-    <input id="jc7" name="" type="checkbox"><label for="jc7">German </label><br>
-    <input id="jc8" name="" type="checkbox"><label for="jc8">History </label><br>
-    <input id="jc9" name="" type="checkbox"><label for="jc9">Home Economics </label><br>
-    <input id="jc10" name="" type="checkbox"><label for="jc10">Irish </label><br>
-    <input id="jc11" name="" type="checkbox"><label for="jc11">Italian </label><br>
-    <input id="jc12" name="" type="checkbox"><label for="jc12">Latin </label><br>
-    <input id="jc13" name="" type="checkbox"><label for="jc13">Materials Technology (wood) </label><br>
-    <input id="jc14" name="" type="checkbox"><label for="jc14">Maths </label><br>
-    <input id="jc15" name="" type="checkbox"><label for="jc15">Metalwork </label><br>
-    <input id="jc16" name="" type="checkbox"><label for="jc16">Music </label><br>
-    <input id="jc17" name="" type="checkbox"><label for="jc17">Science </label><br>
-    <input id="jc18" name="" type="checkbox"><label for="jc18">Spanish </label><br>
-    <input id="jc19" name="" type="checkbox"><label for="jc19">Technical Graphics </label><br>
-    <input id="jc20" name="" type="checkbox"><label for="jc20">Technology </label><br>
-</div>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap stylesheets -->
+    <title>TutorMe Login</title>
+	<!-- To incorporate Lato - a Google font -->
+	 <link href='http://fonts.googleapis.com/css?family=Lato:400,700' rel='stylesheet'  type='text/css'>
 
-<center><input type="submit" name="register-submit" value="Register" id="reg-submit" class="btn btn-success "></center>
-</form>
--->
+	<!-- For social media buttons -->
+	<!-- The stylesheet where we add our own stylings -->
 
-                      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-                      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-                      <script src="js/bootstrap-select.js"></script>
-</body>
+  <link rel='stylesheet prefetch' href='http:////netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css'>
+
+
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+  <link rel="stylesheet" href="css/bootstrap-select.css">
+  <link rel="stylesheet" href="css/my_style.css">
+  <link rel="stylesheet" href="social_media/css/button_style.css">
+  <link rel="stylesheet" href="css/animate.css">
+  <link rel="stylesheet" href="css/checkbox.css">
+
+
+</head>
--- checkbox_testForm.php
+++ checkbox_testForm.php
@@ -15,62 +15,11 @@ if(isset($_POST['submit']))
   $theUserID = "2";
   $userSession = "joe_bloggs";
 
-  for($i=0;$i <= count; $i++)
+  for($i=0;$i < $count; $i++)
   {
-    $addthem = "INSERT INTO tutor_subjects (user_id, cycle_id, subject_id) VALUES ((SELECT user_id FROM users WHERE username = '$userSession'), '1', '$answer')";
+
+    $addthem = "INSERT INTO tutor_subjects (user_id, cycle_id, subject_id) VALUES ((SELECT user_id FROM users WHERE username = '$userSession'), '1', '$answer[$i]')";
+    mysqli_query($conn, $addthem);
   }
-
-  //$jc_insert = "UPDATE user_tutor SET jc_subjects = (SELECT subject_name FROM Subjects WHERE subject_id='$answer') WHERE user_id='$theUserID'";
-
-  //mysqli_query($conn, $jc_insert);
-
 }
 ?>
-
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- Bootstrap stylesheets -->
-    <title>TutorMe Login</title>
-	<!-- To incorporate Lato - a Google font -->
-	 <link href='http://fonts.googleapis.com/css?family=Lato:400,700' rel='stylesheet'  type='text/css'>
-
-	<!-- For social media buttons -->
-	<!-- The stylesheet where we add our own stylings -->
-
-  <link rel='stylesheet prefetch' href='http:////netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css'>
-
-
-  <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-  <link rel="stylesheet" href="css/bootstrap-theme.min.css">
-  <link rel="stylesheet" href="css/bootstrap-select.css">
-  <link rel="stylesheet" href="css/my_style.css">
-  <link rel="stylesheet" href="social_media/css/button_style.css">
-  <link rel="stylesheet" href="css/animate.css">
-  <link rel="stylesheet" href="css/checkbox.css">
-
-
-</head>
-<body style="background:#dbdfe5">
-
-
-<form action="" method="POST" style="">
-<?php include("checkbox_test.php");
- ?>
-
-<center><input onClick="" type="submit" value="Submit" name="submit"></center>
-</form>
-
-<script>
-var sauce = "<?php echo $test; ?>";
-
-function check()
-{
-  alert(sauce);
-}
-</script>
-
-</body>
