1dab91fc97321b36519d32a4039464c852a2cd99
james burke <james burke>
2017-02-21 16:05:12 +0000

    Profile

---
+++ Profile.php
@@ -0,0 +1,215 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- Bootstrap stylesheets -->
+  <title>$Name Profile</title>
+<!-- To incorporate Lato - a Google font -->
+ <link href='http://fonts.googleapis.com/css?family=Lato:400,700' rel='stylesheet'  type='text/css'>
+
+<!-- For social media buttons -->
+<!-- The stylesheet where we add our own stylings -->
+<link rel='stylesheet prefetch' href='http:////netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css'>
+
+<style type="text/css">
+.tg  {border-collapse:collapse;padding-top: 10px;border-spacing:0;}
+.tg td{font-family:Arial, sans-serif;font-size:22px;margin-left: 25px;padding-top: 10px;background-color:#ededed;padding:18px 40px;border-style:solid;border-width:2px;overflow:hidden;word-break:normal;}
+.tg th{font-family:Arial, sans-serif;font-size:22px;font-weight:normal;padding-top: 10px;padding:18px 40px;background-color:#ededed;border-style:solid;border-width:2px;overflow:hidden;word-break:normal;}
+.tg .tg-h31u{font-family:Arial, Helvetica, sans-serif !important;;vertical-align:top}
+.tg .tg-vyiz{background-color:#ededed;vertical-align:top}
+.tg .tg-yw4l{vertical-align:top}
+</style>
+
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+<link rel="stylesheet" href="css/my_style.css">
+<link rel="stylesheet" href="social_media/css/button_style.css">
+<link rel="stylesheet" href="css/animate.css">
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+</head>
+<body style="background:#dbdfe5">
+  <nav id="myNavbar" class="navbar navbar-default navbar-inverse navbar-fixed-top" role="navigation">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbarCollapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+
+			<a href="index.html"><img src="images/smallLogoWhite.png" style="padding-right:50px"></a>
+        </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="index.html" target="_blank">Home</a></li>
+                <li><a href="aboutus.html">About</a></li>
+                <li><a href="contactus.php">Contact</a></li>
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown">How it Works <span class="caret"></span></a>
+                  <ul style="" class="dropdown-menu">
+                    <li><a href="student_how.html">For a student</a></li>
+                    <li><a href="tutor_how.html">For a tutor</a></li>
+                  </ul>
+                </li>
+            </ul>
+			<ul class="nav navbar-nav navbar-right">
+        <!-- <li><a href="index.html"><span style="padding-right:10px" class="glyphicon glyphicon-log-in"></span>Login</a></li> -->
+        <li><a href="register.html" class=""><span style="padding-right:10px" class="glyphicon glyphicon-user"></span>Register</a></li>
+        <li class="dropdown">
+    <a href="#" class="" data-toggle="dropdown"><span style="padding-right:10px" class="glyphicon glyphicon-log-in"></span>Login</a>
+    <ul class="dropdown-menu dropdown_width animated slideInRight" style="width:350px" role="menu">
+    </ul>
+</li>
+
+        </div>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<!--Profile Picture, credentials and about section-->
+
+<div class="container-fluid">
+    <div style="padding-top:40px;padding-bottom:40px;background:url('images/blurred-background-4.jpg');background-size:cover;" class="row">
+<div style="margin-left:15px;padding-top:35px;text-align:center;" class="col-sm-6 col-md-4 col-lg-4;">
+  <img src="http://d235mwrq2dn9n5.cloudfront.net/wp-content/uploads/2016/11/rtr_kanye_west_jc_150407_16x9_992-1.jpg" alt="Profile Pic" style = "width:500px;height500px;" align="left;" />
+  <h1>Kanye West</h1>
+  <a target="_blank"  class="btn btn-success btn-lg">Message Tutor</a>
+  </div>
+<div class="col-sm-6 col-md-4 col-lg-4">
+  <div style = "margin-left:15px;padding-top:10px;">
+<h2><b><u><font color = "#ededed">About $name</font></b></u></h2>
+    </div>
+  </div>
+    <div class="col-sm-6 col-md-7 col-lg-7">
+      <div style = "margin-left:15px;">
+      <font color = "black"><p>LookIf you hadOne shotOr one opportunityTo seize everything you ever wantedIn one momentWould you capture itOr just let it slip?
+YoHis palms are sweaty, knees weak, arms are heavyThere's vomit on his sweater already,
+mom's spaghettiHe's nervous, but on the surface he lookscalm and readTo drop bombs, but he keeps on forgettin'What he wrote down, the whole crowd goes so loudHe opens his mouth, but the words won't come outHe's chokin', how, everybody's jokin' nowThe clocks run out, times up, over, blaow!Snap back to reality, oh there ges gravityOh, there goes Rabbit, he chokedHe's so mad, but he won't give up that easy? NoHe won't have it, he knows his whole back city's ropesIt don't matter, he's dope, he knows that, but he's bro</p></font>
+    </div>
+  </div>
+    <div class="col-sm-6 col-md-7 col-lg-7">
+      <div style = "margin-left:15px;">
+    <h2><b><u><font color = "#ededed">Experience/Credentials</font></u></b></h2>
+    </div>
+  </div>
+    <div class="col-sm-6 col-md-7 col-lg-7">
+      <div style = "margin-left:15px;">
+    <font color = "black"><p>Oh I wish I was a punk rocker with flowers in my hair In 77 and 69 revolution was in the airI was born too late to a world that doesn't careOh I wish I was a punk rocker with flowers
+      in my hairWhen the head of state didn't play guitar,Not everybody drove a carWhen music really mattered and radio was king,When accountants didn't have controlAnd the media couldn't buy your soulAnd computers were still scary and we didn't know everythingOh I wish I was a punk rocker with floers in my hairIn 77 and 69 revolution was in the airI was born too late to a world that doesn't careOh I wish I was a punk rocker with flowers in m</p><font>
+    </div>
+  </div>
+  </div>
+</div>
+<!--<div style="margin-left:25px;padding-top:65px;">
+<img src="http://d235mwrq2dn9n5.cloudfront.net/wp-content/uploads/2016/11/rtr_kanye_west_jc_150407_16x9_992-1.jpg" alt="Profile Pic" style = "width:500px;height500px;" />
+<h2>About $name</h2>
+</div>
+-->
+
+<!--Table for subjects, location, etc-->
+
+<div class="col-sm-6 col-md-4 col-lg-4">
+<div style="text-align:center">
+  <h4>Test</h4>
+  <table class="tg" align="center">
+  <tr>
+    <td class="tg-yw4l"><b>Subject(s)</b></td>
+    <td class="tg-yw4l">Geography (JC)</td>
+  </tr>
+  <tr>
+    <td class="tg-yw4l"><b>Location</b></td>
+    <td class="tg-yw4l">$Location</td>
+  </tr>
+  <tr>
+    <td class="tg-yw4l"><b>Qualifications</b></td>
+    <td class="tg-yw4l">$Qualifications</td>
+  </tr>
+  <tr>
+    <td class="tg-yw4l"><b>Price</b></td>
+    <td class="tg-yw4l">$Price</td>
+  </table>
+  </tr>
+  </div>
+</div>
+  <div class="col-sm-6 col-md-4 col-lg-4" style="padding-left:10px;padding-right:10px;text-align:center;">
+<h4>Find $Name on the map</h4>
+  <!-- Add the Google Map in Here -->
+  	<iframe width="100%" height="280" frameborder="0" scrolling="yes" marginheight="0" marginwidth="0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2379.636004935249!2d-6.259728683839165!3d53.38556197998734!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48670e113b293d85%3A0xc81174de93fa22d6!2sSchool+of+Computing%2C+Dublin+City+University!5e0!3m2!1sen!2sie!4v1480694368387"></iframe>
+  </div>
+
+  <div class="col-sm-6 col-md-4 col-lg-4" style="padding-left:10px;padding-right:10px;text-align:center;">
+<h4>Rating</h4>
+<img src="images\5star.png" alt="Profile Pic" height="280">
+</div>
+
+
+  </div>
+
+
+
+<div class="container-fluid" style="background-color:grey;background-size:cover;">
+  <?php //include($root . "content_pages/footer.html"); ?>
+</div>
+<!--
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
+-->
+
+<div class="container-fluid" style="margin-bottom:20px;">
+	<div style="border-top:1px solid #dbdfe5" class="row">
+		<div style="border-left:1px solid #dbdfe5" class="col-xs-4">
+		<br>
+		<h3 style="text-align:center;font-family: 'Lato', sans-serif;">Connect with us.</h3>
+		<center><ul style="padding-top:30px;" class="social-icons icon-rounded icon-zoom  list-unstyled list-inline">
+		<li> <a href="#"><i class="fa fa-facebook"></i></a></li>
+		<li> <a href="#"><i class="fa fa-linkedin"></i></a></li>
+		<li> <a href="#"><i class="fa fa-twitter"></i></a></li>
+		</ul></center>
+		<p style="text-align:center"><br>&#169; TutorMe, Mark Gerrity, James Burke</p>
+		</div>
+
+		<div style="border-left:1px solid #dbdfe5" class="col-xs-4">
+      <h3 style="text-align:center;padding-top:30px;font-family: 'Lato', sans-serif;">Navigation</h3>
+      <ul style="padding-top:30px;text-align:center" class="list-unstyled list-inline">
+          <li> <a class="footer_links" style="padding-right:10px;border-right: 1px solid #646464" href="index.html">Home</a></li>
+        <li> <a class="footer_links" style="padding-right:10px;border-right: 1px solid #646464" href="aboutus.html">About</a></li>
+        <li> <a class="footer_links" href="contactus.php">Contact</a></li>
+      </ul>
+
+      <ul style="padding-top:10px;text-align:center" class="list-unstyled list-inline">
+        <li> <a class="footer_links" style="padding-right:10px;border-right: 1px solid #646464" href="student_how.html">How it works</a></li>
+        <li> <a class="footer_links" style="padding-right:10px;border-right: 1px solid #646464" href="index.html">Login</a></li>
+        <li> <a class="footer_links" style="" href="register.html">Register</a></li>
+      </ul>
+
+		</div>
+
+		<div style="border-left:1px solid #dbdfe5" class="col-xs-4">
+        <center><img src="images/Logo.png"></center>
+        <center><ul style="padding-top:0px;" class="list-unstyled list-inline">
+        <li> <a class="footer_links" style="padding-right:10px;border-right: 1px solid #646464" href="sitemap.html">Sitemap</a></li>
+        <li> <a class="footer_links" style="padding-right:10px;border-right: 1px solid #646464" href="privacy.html">Privacy Policy</a></li>
+        <li> <a class="footer_links" href="service_terms.html">Terms of Service</a></li>
+        </ul></center>
+		</div>
+	</div>
+</div>
+
+
+
+</body>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
+<script type="text/javascript" src="js/contact.js"></script>
+<script type="text/javascript" src="js/mapSettings.js"></script>
+
+</html>
